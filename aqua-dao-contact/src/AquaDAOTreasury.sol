11ece276a76c195982c066bc157f6615a86be7fe
kavinda-100 <kavindarathnayake100@gmail.com>
2025-08-19 11:59:16 +0530

    Implement AquaDAOTreasury contract with ownership and ETH transfer functionality

--- aqua-dao-contact/src/AquaDAOTreasury.sol
+++ aqua-dao-contact/src/AquaDAOTreasury.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
 /**
  * @title Aqua Treasury
  * @author Kavinda Rathnayake
@@ -8,6 +10,26 @@ pragma solidity ^0.8.24;
  * @dev The AquaDAOTreasury contract is responsible for managing the funds of the Aqua DAO.
  * @dev This contract holds the funds of users paid when they buy the Aqua Governance Token.
  */
-contract AquaDAOTreasury {
-    constructor() {}
+contract AquaDAOTreasury is Ownable {
+    // ------------------------------------ Errors ----------------------------
+    error NotEnoughETH();
+    error ETHTransferFailed();
+
+    constructor() Ownable(msg.sender) {}
+
+    /**
+     * @dev Sends ETH from the treasury to a specified address.
+     * @param _to The address to send ETH to.
+     * @param _amount The amount of ETH to send.
+     */
+    function sendETH(address _to, uint256 _amount) external onlyOwner {
+        if (address(this).balance < _amount) revert NotEnoughETH();
+        (bool sent,) = payable(_to).call{value: _amount}("");
+        if (!sent) revert ETHTransferFailed();
+    }
+
+    /**
+     * @dev Fallback function to receive ETH.
+     */
+    receive() external payable {}
 }
