d365b42e1aaee0145afd6af979f4f022ee99a49b
kavinda-100 <kavindarathnayake100@gmail.com>
2025-08-28 19:39:04 +0530

    Add test for ETH transfer from treasury by owner in AquaDAOTreasuryTest

--- aqua-dao-contact/test/unit/AquaDAOTreasuryTest.t.sol
+++ aqua-dao-contact/test/unit/AquaDAOTreasuryTest.t.sol
@@ -44,4 +44,32 @@ contract AquaDAOTreasuryTest is Test {
         // check final treasury balance
         assertEq(address(aquaDAOTreasury).balance, mintCost);
     }
+
+    /**
+     * Tests the ability of the owner to send ETH from the treasury.
+     */
+    function test_sendEth_by_owner() public {
+        address owner = aquaDAOTreasury.owner();
+        address recipient = makeAddr("recipient");
+        uint256 mintAmount = 5;
+        uint256 mintCost = mintAmount * aquaGovToken.getMintPrice();
+        uint256 sendAmount = mintCost / 2;
+
+        // mint tokens
+        vm.startPrank(user1);
+        aquaGovToken.mint{value: mintCost}(mintAmount);
+        vm.stopPrank();
+
+        // check recipient balance
+        assertEq(recipient.balance, 0);
+
+        // send eth to recipient
+        vm.startPrank(owner);
+        aquaDAOTreasury.sendETH(recipient, sendAmount);
+        vm.stopPrank();
+
+        // check final balances
+        assertEq(address(aquaDAOTreasury).balance, mintCost - sendAmount);
+        assertEq(recipient.balance, sendAmount);
+    }
 }
