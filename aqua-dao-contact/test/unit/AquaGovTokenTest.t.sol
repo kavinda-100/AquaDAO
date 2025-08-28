4e05e24fe09a5c19793b889a7743e59414427394
kavinda-100 <kavindarathnayake100@gmail.com>
2025-08-28 18:23:34 +0530

    Add deployment summary JSON and AquaGovToken test contract

---
+++ aqua-dao-contact/test/unit/AquaGovTokenTest.t.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {DeployAquaDAO} from "../../script/DeployAquaDAO.s.sol";
+import {AquaGovToken} from "../../src/AquaGovToken.sol";
+
+contract AquaGovTokenTest is Test {
+    // ---------------------- State Variables ---------------------------------
+    AquaGovToken aquaGovToken;
+    address public user1 = makeAddr("user1");
+    address public user2 = makeAddr("user2");
+    uint256 public initialUserBalance = 10 ether;
+
+    // ---------------------- Set Up ---------------------------------
+    function setUp() public {
+        // deploy AquaGov
+        DeployAquaDAO deployAquaDAO = new DeployAquaDAO();
+        // store the deployed AquaGovToken
+        (, aquaGovToken,) = deployAquaDAO.run();
+        // fund users
+        vm.deal(user1, initialUserBalance);
+        vm.deal(user2, initialUserBalance);
+    }
+
+    // ------------------------ Tests for mint tokens ---------------------------------
+
+    /**
+     * Tests the minting of tokens.
+     */
+    function test_mintTokens() public {
+        uint256 mintAmount = 5; // number of tokens to mint
+        uint256 mintCost = mintAmount * aquaGovToken.getMintPrice(); // cost to mint tokens
+
+        // check initial balance
+        assertEq(aquaGovToken.balanceOf(user1), 0);
+
+        // mint tokens
+        vm.prank(user1);
+        aquaGovToken.mint{value: mintCost}(mintAmount);
+
+        // check final balance
+        assertEq(aquaGovToken.balanceOf(user1), mintAmount);
+    }
+}
