b2fdfc4e4b8d9fa767ad645feba0cf8a84f3a762
kavinda-100 <kavindarathnayake100@gmail.com>
2025-09-04 20:31:21 +0530

    feat: update HowItWorks component with detailed section description

--- aqua-dao-ui/src/sections/HowItWorks.tsx
+++ aqua-dao-ui/src/sections/HowItWorks.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 
+/**
+ * How the DAO works section
+ * 1. By Aqua tokens by paying ETH (Become a member of the DAO)
+ * 2. Create proposals by someone (the one who created the proposal is the    owner)
+ * 3. Others can Vote on proposals (votes for or against)
+ * 4. Execute proposals - (Only Owner of the proposal can execute)
+ * 5. Governance and transparency
+ */
+
 export const HowItWorks = () => {
   return <div>How It Works</div>;
 };
